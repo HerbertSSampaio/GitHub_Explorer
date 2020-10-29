d330aff05a3b597c01cb0a0f03743820a89b861b
HerbertSousa <herberti1405@outlook.com>
2020-10-29 11:02:47 -0300

    Handle Errors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
   full_name: string;
@@ -17,28 +17,42 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
-    event.preventDefault();
+  event.preventDefault();
 
-    const response = await api.get<Repository>(`repos/${newRepo}`);
-    const repository = response.data;
+  if(!newRepo) {
+    setInputError('Digite o autor/nome do Repositório');
+    return;
+  }
+try {
+  const response = await api.get<Repository>(`repos/${newRepo}`);
+  const repository = response.data;
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+  setRepositories([...repositories, repository]);
+  setNewRepo('');
+  setInputError('');
+} catch(err){
+  setInputError('Erro na busca por esse repositório');
+}
+
   }
 
   return (
     <>
     <img src={logoImg} alt="GitHub Explorer" />
   <Title>Explore Repositórios no GitHub</Title>
-  <Form onSubmit={handleAddRepository}>
+  <Form hasError={ !! inputError} onSubmit={handleAddRepository}>
     <input
     value={newRepo}
     onChange={e => setNewRepo(e.target.value)}
     placeholder="Digite o nome do repositório" />
     <button type="submit">Pesquisar</button>
   </Form>
+
+  { inputError && <Error>{inputError}</Error> }
+
   <Repositories>
     {repositories.map(repository => (
     <a key={repository.full_name} href="#">
